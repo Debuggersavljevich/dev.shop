ec8536ea36c05037d8b237a7cfbc67b992dcbbc9
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-07-06 22:59:18 -0300

    primera instancia de la app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='bg-teal-700 w-screen py-4 font-bold text-xl text-white flex justify-between'>
+        <p className='font-mono ml-5'>dev.shop</p>
+        <nav className='font-mono inline-block'>
+            <ul className='mr-5'>
+                <li>Shop</li>
+                <li>test</li>
+            </ul>
+        </nav>
+        
+    </div>
+
+       
+    
+    </>
+  )
+}
+
+export default Navbar
